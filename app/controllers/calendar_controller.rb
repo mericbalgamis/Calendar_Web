b53e71f2b4c5e70b9085f72f08be66a3295e5732
Ibrahim Burak Sahin <burakshn96@gmail.com>
2018-07-03 12:24:57 +0300

    Repo clean up.

--- app/controllers/calendar_controller.rb
+++
@@ -1,7 +0,0 @@
-class CalendarController < ApplicationController
-  def show
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
-
-
-  end
-end
--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-CalendarExample::Application.routes.draw do
-  resource :calendar, only: [:show], controller: :calendar
-  root to: "calendar#show"
-end
